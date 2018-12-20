0d233f009f4f464f4d65fce869650ed8f9026a7c
Naveen Kumar <nkr4@3ds.com>
2018-12-20 12:43:07 +0530

    improved socket

--- socket/socket.js
+++ socket/socket.js
@@ -10,14 +10,15 @@ exports.init = function (server) {
     io = sio(server);
     io.on('connection', function (socket) {
         console.log('a user connected');
-        io.emit("Hey!\nHow can I help you?")
         socket.on('disconnect', function () {
             console.log('user disconnected');
         });
         socket.on('message', function (msg) {
             console.log('message: ' + msg.message);
             console.log('user: ' + msg.user);
+            console.log('last user: ' + msg.lastUser);
             io.emit('message', msg);
+            msg.lastUser = msg.user;
         });
     });
 };
